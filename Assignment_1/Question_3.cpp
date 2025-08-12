1b6e184314e18369de5d47d3d68b0373aff53333
tgoyal1be24 <tgoyal1_be24@thapar.edu>
2025-08-12 12:00:10 +0530

    Question_3.cpp

--- Assignment_1/Question_3.cpp
+++ Assignment_1/Question_3.cpp
@@ -1,3 +1,11 @@
+/*3) Predict the Output of the following program
+int main()
+{
+int i;
+int arr[5] = {1};
+for (i = 0; i < 5; i++) printf("%d",arr[i]);
+return 0;
+}*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,4 +17,5 @@ int main()
         printf("%d", arr[i]);
     return 0;
 }
+
 // Output : 10000
